340bf1c1d0b65f56018c09967d359102a975e527
Qori El-Hafizh <contact@qori.dev>
2023-02-10 18:41:46 +0700

    clustering and port env

--- src/framework/runApp.ts
+++ src/framework/runApp.ts
@@ -5,38 +5,42 @@ import Express from "express";
 import { jsxViewEngine } from "./jsx-view-engine";
 
 export function runApp(
-  callback: (express: Express.Express) => void,
-  { port }: { port: number }
+  { port, clustering }: { port: number | string; clustering: boolean },
+  callback: (express: Express.Express) => void
 ) {
-  if (cluster.isPrimary) {
-    const totalCpus = os.cpus().length;
-
-    for (let i = 0; i < totalCpus; i++) {
-      cluster.fork();
-    }
-
-    console.log(`http://localhost:${port}`);
-
-    cluster.on("exit", (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
-      console.log("Let's fork another worker!");
-      cluster.fork();
-    });
+  if (clustering && cluster.isPrimary) {
+    startWorkers();
   } else {
-    const express = Express();
-
-    express.use(
-      "/static",
-      Express.static(path.join(__dirname, "..", "static"))
-    );
-    express.use("/public", Express.static("./public"));
-
-    express.engine("js", jsxViewEngine);
-    express.set("views", path.resolve(__dirname, "..", "views"));
-    express.set("view engine", "js");
-
+    const express = createExpress();
     callback(express);
-
     express.listen(port);
   }
+}
+
+function createExpress() {
+  const express = Express();
+
+  express.use("/static", Express.static(path.join(__dirname, "..", "static")));
+  express.use("/public", Express.static("./public"));
+
+  express.engine("js", jsxViewEngine);
+  express.set("views", path.resolve(__dirname, "..", "views"));
+  express.set("view engine", "js");
+
+  return express;
+}
+
+function startWorkers() {
+  const totalCpus = os.cpus().length;
+
+  for (let i = 0; i < totalCpus; i++) {
+    console.log(`create worker ${i}`);
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { runApp } from "./framework/runApp";
 
 runApp(
+  {
+    port: process.env.PORT || 8080,
+    clustering: process.env.CLUSTERING === "true" || false,
+  },
   (express) => {
     express.get("/", async (req, res, next) => {
       res.render("HomePage", { title: "hello" });
     });
 
     express.get("/cat", (req, res) => res.send("meow"));
-  },
-  { port: 8080 }
+  }
 );
